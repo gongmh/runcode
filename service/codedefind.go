da318c831c5cc809014581479a1189775278025e
gongmh <pandergong@gmail.com>
2020-06-05 23:22:14 +0800

    :wrench: add lua run env
    add lua run env

--- service/codedefind.go
+++ service/codedefind.go
@@ -22,6 +22,7 @@ var goDemoCode = "package main\n\nfunc main() { \n    println(\"hello golang\")\
 var javaDemoCode = "import java.io.*;\nclass test {\n    public static void main (String[] args) throws java.lang.Exception {\n        System.out.println(\"hi java\");\n    }\n}"
 var pyDemoCode = "#!/usr/bin/python\n\nprint(\"hello python\")"
 var cppDemoCode = "#include <iostream>\n\nint main(){\n    std::cout << \"hello cpp\" << std::endl;\n}"
+var luaDemoCode = "print(\"hello world\")"
 
 //docker命令，格式：1. 先创建容器；2.copy文件到容器；3. 运行容器执行命令
 var golangCmdV2 = `cid=$(docker create -c 512 -m 512M --rm golang:alpine sh -c "go build /tmp/1.go && ./1 2>> tmp && cat tmp") && docker cp %s $cid:/tmp/1.go && docker start $cid -a`
@@ -29,6 +30,7 @@ var phpCmdV2 = `cid=$(docker create -c 512 -m 512M --rm php sh -c "/usr/local/bi
 var javaCmdV2 = `cid=$(docker create -c 512 -m 512M --rm java sh -c "javac /tmp/1.java && sh /tmp/javaRunner.sh") && docker cp ./scripts/javaRunner.sh $cid:/tmp/javaRunner.sh && docker cp %s $cid:/tmp/1.java && docker start $cid -a`
 var cppCmdV2 = `cid=$(docker create -c 512 -m 512M --rm -w /tmp gcc:4.9 sh -c "g++ -o myapp 1.cpp && ./myapp") && docker cp %s $cid:/tmp/1.cpp && docker start $cid -a`
 var pythonCmdV2 = `cid=$(docker create -c 512 -m 512M --rm python:2.7 sh -c "python /tmp/1.py") && docker cp %s $cid:/tmp/1.py && docker start $cid -a`
+var luaCmdV2 = `cid=$(docker create -c 512 -m 512M --rm luafan/luafan-alpine sh -c "lua /tmp/1.lua") && docker cp %s $cid:/tmp/1.lua && docker start $cid -a`
 
 //代码整体配置
 //codemirror mode: https://codemirror.net/mode/index.html
@@ -72,5 +74,13 @@ var codeShowMap = map[string]CodeStruct{
 		"text/x-c++src",
 		cppCmdV2,
 		".cpp",
+	},
+	"lua": {
+		"lua",
+		"lua",
+		luaDemoCode,
+		"text/x-lua",
+		luaCmdV2,
+		".lua",
 	},
 }
